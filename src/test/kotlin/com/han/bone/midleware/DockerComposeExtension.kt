dcc7e189eaae510bffc3849f598c303da70a4f6e
han0001 <hsw9494@gmail.com>
2025-08-31 22:13:07 +0900

    TestContainer 부분설정 가능

--- src/test/kotlin/com/han/bone/midleware/DockerComposeExtension.kt
+++ src/test/kotlin/com/han/bone/midleware/DockerComposeExtension.kt
@@ -1,27 +1,26 @@
 package com.han.bone.midleware
 
 import com.han.bone.util.logging.log
-import io.kotest.core.listeners.AfterProjectListener
-import io.kotest.core.listeners.BeforeProjectListener
-import io.kotest.core.spec.AutoScan
+import io.kotest.core.listeners.AfterSpecListener
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.spec.Spec
 import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.wait.strategy.ShellStrategy
 import java.io.File
 
-@AutoScan
-class DockerComposeExtension : BeforeProjectListener, AfterProjectListener {
+
+class DockerComposeExtension : BeforeSpecListener, AfterSpecListener {
 
     private val container = DockerComposeContainer<Nothing>(File("src/test/resources/docker-compose.yml")).apply {
-//        waitingFor("mysql_for_test", ShellStrategy().withCommand("mysql -u'test' -p'123456' -e'select 1' && sleep 2")) // 접속 가능해질때까지 대기
         waitingFor("redis_for_test", ShellStrategy().withCommand("redis-cli get test"))
     }
 
-    override suspend fun beforeProject() {
+    override suspend fun beforeSpec(spec: Spec) {
         log.info { "[DOCKER_COMPOSE_EXTENSION] start" }
         container.start()
     }
 
-    override suspend fun afterProject() {
+    override suspend fun afterSpec(spec: Spec) {
         container.stop()
         log.info { "[DOCKER_COMPOSE_EXTENSION] stop" }
     }
--- src/test/kotlin/com/han/bone/midleware/redis/RedisManagerTest.kt
+++ src/test/kotlin/com/han/bone/midleware/redis/RedisManagerTest.kt
@@ -1,4 +1,5 @@
 package com.han.bone.midleware.redis
+import com.han.bone.midleware.DockerComposeExtension
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest
 class RedisManagerTest @Autowired constructor(
     private val redisManager: RedisManager
 ) : FunSpec({
+
+    extension(DockerComposeExtension())
 
     test("RedisManager Test") {
         val key = "test-key"
